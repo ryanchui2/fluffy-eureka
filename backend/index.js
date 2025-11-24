a51ad688965fa0647ef4774299eb99933c962683
ryanchui2 <ryanhuichun0201@gmail.com>
2025-11-24 02:20:49 -0500

    credential update

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 app.use(cors(
     {
         origin: FRONTEND_URL,
+        credentials: true
     }
 ));
 app.use(express.json());
